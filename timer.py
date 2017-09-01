045995439bba3bcde9bdc6fdd3d31de088fc5536
peteflorence <pete.florence@gmail.com>
2017-09-01 10:22:09 -0400

    timer to screen

--- timer.py
+++ timer.py
@@ -1,5 +1,7 @@
+from __future__ import print_function
 import os
 import time
+import sys
 
 def beginThing(thing):
   os.system("say Now begin ... ")
@@ -9,17 +11,28 @@ def beginThing(thing):
 def minutesToSec(minutes):
   return minutes*60
 
-interval_minutes = 0.1
+interval_minutes = 15
 interval = minutesToSec(interval_minutes)
 
-beginThing("Run")
-time.sleep(interval)
+def timeAndPrint(interval):
+  for i in range(int(interval)):
+    seconds_remaining = interval - i
+    minutes_remaining = seconds_remaining / 60
+    seconds_remainder = seconds_remaining % 60
+    time.sleep(1)
+    sys.stdout.write('\r' + str(minutes_remaining).zfill(2) + ':' + str(seconds_remainder).zfill(2) +' ' * 20)
+    sys.stdout.flush() # important
+  sys.stdout.write('\r' + ' ' * 20)
+  print()
+
 beginThing("Think about PhD")
-time.sleep(interval)
+timeAndPrint(interval)
 beginThing("Read latest research")
-time.sleep(interval)
+timeAndPrint(interval)
 beginThing("Read latest tech / business")
-time.sleep(interval)
+timeAndPrint(interval)
 beginThing("Reed software blog")
-time.sleep(interval)
+timeAndPrint(interval)
 beginThing("Improve this system")
+timeAndPrint(interval)
+beginThing("Run")
