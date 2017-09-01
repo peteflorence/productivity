e1c38053814b8b6f9114ad9078b41f523c626ef7
peteflorence <pete.florence@gmail.com>
2017-09-01 13:57:23 -0400

    this morning updates -- workouts, etc

--- timer.py
+++ timer.py
@@ -2,6 +2,12 @@ from __future__ import print_function
 import os
 import time
 import sys
+from random import shuffle
+
+# TODO
+# Work in workouts in between
+# Text alerts with Twilio?
+# Consider browser extension?
 
 def beginThing(thing):
   os.system("say Now begin ... ")
@@ -13,7 +19,6 @@ def minutesToSec(minutes):
 
 interval_minutes = 15
 interval = minutesToSec(interval_minutes)
-interval = 5
 
 def timeAndPrint(interval):
   for i in range(int(interval)):
@@ -30,25 +35,41 @@ def printSuggestions(list_things):
   for i in list_things:
     print(i)
 
+workouts = ['pushups', 'judo pushups', 'leg raises', 'Y squats', 'stretch']
+shuffle(workouts) 
+workout_counter = 0
+def provideWorkout(seconds):
+  global workout_counter
+  workout = workouts[workout_counter]
+  workout_counter += 1
+  print("say WORKOUT: " + workout)
+  os.system("say WORKOUT: " + workout)
+  timeAndPrint(seconds)
+
 beginThing("Think about PhD")
 printSuggestions(['Write on piece of paper...'])
 timeAndPrint(interval)
+provideWorkout(60)
 
 beginThing("Read latest research")
 printSuggestions(['arXiv sanity...', 'maybe also go through conference proceedings'])
 timeAndPrint(interval)
+provideWorkout(60)
 
 beginThing("Read latest tech / business")
 printSuggestions(['ieee spectrum', 'techrunch', 'nytimes bits blog'])
 timeAndPrint(interval)
+provideWorkout(60)
 
 beginThing("Reed software blog")
 printSuggestions(['joel on software', 'martin fowler', 'karpathy'])
 timeAndPrint(interval)
+provideWorkout(60)
 
 beginThing("Improve this system")
 printSuggestions(['write on piece of paper', 'implement'])
 timeAndPrint(interval)
+provideWorkout(60)
 
 beginThing("Run")
 timeAndPrint(interval)
